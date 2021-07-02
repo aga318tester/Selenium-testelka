ff29e9b913b44747855de3ef770716f082c4d760
aga318tester <agusia031@wp.pl>
2021-07-02 09:39:40 +0200

    02.07

--- src/test/java/Projekt_Testowy_Selenium/Projekt_Testowy_Selenium/opinie.java
+++ src/test/java/Projekt_Testowy_Selenium/Projekt_Testowy_Selenium/opinie.java
@@ -1,5 +1,5 @@
 package Projekt_Testowy_Selenium.Projekt_Testowy_Selenium;
-import java.util.concurrent.TimeUnit;
+
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,9 +11,12 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import net.bytebuddy.implementation.bind.annotation.Super;
 
 
@@ -37,8 +40,7 @@ public class opinie extends zakladanie_konta{
 		// wybór wycieczki
 		
 		driver.findElement(By.linkText("Yoga i pilates")).click();
-	//	driver.findElement(By.xpath("?add-to-cart=60\" data-quantity=\"1")).click();
-		//driver.findElement(By.name("orderby")).click();
+	
 		
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,250)", "");
@@ -48,6 +50,37 @@ public class opinie extends zakladanie_konta{
 		
 		
 		// przejście na opinie i jej dodanie
+		js.executeScript("window.scrollBy(0,550)", "");
+		
+		driver.findElement(By.cssSelector("#tab-title-reviews > a")).click();
+		
+//generowanie randomowego numer
+		 
+		//losowanie_random();
+		Random objGenerator = new Random();
+	    for (int iCount = 0; iCount< 1; iCount++){
+	      int randomNumber = objGenerator.nextInt(100000000);
+	  //    System.out.println("Random No : " + randomNumber); 
+		
+          WebElement element = driver.findElement(By.cssSelector("#comment"));
+          Actions actions = new Actions(driver);
+          actions.moveToElement(element);
+          actions.perform();
+		
+          
+		// wystawianie gwiazdek w opinii
+    //     js.executeScript("window.scrollBy(0,500)", "");
+	     driver.findElement(By.cssSelector("#commentform > div > p > span > a.star-5")).click();
+	//	js.executeScript("window.scrollBy(0,400)", "");
+		driver.findElement(By.cssSelector("#comment")).click();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+	   driver.findElement(By.cssSelector("#comment")).sendKeys("było super"+randomNumber);
+   
+	   js.executeScript("window.scrollBy(0,500)", "");
+	   driver.findElement(By.id("submit")).click();
+        }
+		
 	}
 
 	@Before
@@ -68,10 +101,15 @@ public class opinie extends zakladanie_konta{
 	}
 
 
-		
-	
+public void losowanie_random()
+{
+	Random objGenerator = new Random();
+    for (int iCount = 0; iCount< 1; iCount++){
+      int randomNumber = objGenerator.nextInt(100000000);
+  //    System.out.println("Random No : " + randomNumber); 
 
 }
+}}
 
 
 
